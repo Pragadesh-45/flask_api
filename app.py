b7296261533304f91cbf3a8339d8f624a8d37d35
Pragadesh-45 <54320162+Pragadesh-45@users.noreply.github.com>
2022-09-19 06:54:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,53 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 students = [{
     'name': "Fathima",
-    'id': "1",
+    'student_id': "1",
     'class': "1A1",
     'subject': "Environmental Science" },
     {
     'name': "Vijay",
-    'id': "2",
+    'student_id': "2",
     'class': "4A2",
     'subject': "Science" },
     {
     'name': "Antony",
-    'id': "3",
+    'student_id': "3",
     'class': "12A1",
     'subject': "Computer Science" }]
 
 
 @app.route('/')
 def index():
-    return "Welcome to the Index page"
+    return "Welcome To Index Page"
+
+@app.route("/students", methods = ['GET'])
+def get():
+    return jsonify({'Students':students})
+
+@app.route("/students/<int:course_id>", methods = ['GET'])
+def get_student(student_id):
+    return jsonify({'Student':student[student_id]})
+
+@app.route("/students", methods=['POST'])
+def create ():
+    student = {
+    'name': "Pablo Emilio Escobar Gaviria",
+    'student_id': "4",
+    'class': "12A1",
+    'subject': "Drugs" }
+
+    students.append(student)
+    return jsonify({'Created': student})
+
+
+@app.route("/students/<int:student_id>", methods = ['PUT'])
+def student_update(student_id):
+    students[student_id]['subject'] = "Biology"
+    return jsonify({'student': students[student_id]})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
